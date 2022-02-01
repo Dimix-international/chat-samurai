7a8ba2357b3aae74157b447f225e18946d2d1c0d
Dimix-international <dima.dimix94@mail.ru>
2022-02-01 15:19:32 +0300

    added config

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,19 @@ import { Server } from "socket.io";
 
 const app = express();//приложение
 const httpServer = createServer(app);//сервер
-const io = new Server(httpServer, { /* options */ });//socket
+const socket = new Server(httpServer, {
+    cors: {
+        origin: "https://chat-samurai.herokuapp.com"
+    }
+});//socket
 
 app.get('/', (req, res) => {
     res.send('Hello it is WS server'); //создали endpoint
 })
 
-io.on("connection", (socket) => {
-    // ...
+socket.on("connection", (connection) => {
+    // socket подписался на событие connection, и когда оно произойдет выполняется этот код
+    console.log(' a user connected')
 });
 
 const PORT = process.env.PORT || 3009;
