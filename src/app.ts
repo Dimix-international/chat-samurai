270ba985cc050760513edd979c1faa1bfd24e439
Dimix-international <dima.dimix94@mail.ru>
2022-02-02 14:50:55 +0300

    init server

--- src/app.ts
+++ src/app.ts
@@ -16,25 +16,52 @@ const socket = new Server(httpServer, {
     }
 });//socket
 
-/*app.use(
-    cors({
-        origin: "http://localhost:3000",
-        methods: 'GET,POST,PUT,DELETE',
-        credential: true,
-    })
-)*/
+const messages = [
+    {
+        message: 'Hi Dima!! =)',
+        id: 'fe3432',
+        user: {id: 'sdsds33', name: 'Victor'}
+    },
+    {
+        message: 'Hi Victor',
+        id: 'fe3232wds32',
+        user: {id: 'scsc3434', name: 'Dima'}
+    },
+    {
+        message: 'Hi Vika eee!',
+        id: 'fe3232wds32',
+        user: {id: 'scsc3434', name: 'Dima'}
+    },
+    {
+        message: 'Hi Vika eee!',
+        id: 'fe3232wds32',
+        user: {id: 'scsc3434', name: 'Dima'}
+    },
+]
 
 app.get('/', (req, res) => {
     res.send('Hello it is WS server'); //создали endpoint
 })
 
-socket.on("connection", (connection) => {
+socket.on("connection", (socketChannel) => {
     // socket подписался на событие connection, и когда оно произойдет выполняется этот код
+
+    //socketChannel - сокет канал с конкретным пользователем
+    // дальнейшие подписки идут на него
+    socketChannel.on("client-message-sent", (message:string) => {
+        // socket подписался на событие client-message-sent, и когда оно произойдет выполняется этот код
+        console.log(message)
+    });
+
+    //пользователь connected, пришелем ему те сообщения которые у нас уже есть messages[]
+    socketChannel.emit('init-messages-published', messages);
+
     console.log(' a user connected')
 });
 
-const PORT = process.env.PORT || 3009;
+
+const PORT = process.env.PORT || 4009;
 
 httpServer.listen(PORT, () =>{
-    console.log('listening 3009...')
+    console.log(`listening => http://localhost:${PORT}`)
 });
